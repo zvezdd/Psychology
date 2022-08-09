009d0b8b7aaac2eadfa4f040a38641beead2c569
zvezdd <qwiszvezd@gmail.com>
2022-08-10 02:56:47 +0600

    Psychology
    Site about Psychology and problems that psychology can solve.

---
+++ watch.js
@@ -0,0 +1,13 @@
+const anchors = document.querySelectorAll('a[href*="#"]');
+for ( let anchor of anchors) {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const blockID = anchor.getAttribute('href')
+        document.querySelector('' + blockID).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        })
+    })
+}
+
+new WOW().init();
